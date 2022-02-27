5c66baa98e30bfadc19217844bdedbdb94e7a1fc
User2 <User2Email@mail.ru>
2022-02-27 16:42:08 +0300

    SecondStep

---
+++ Task3.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Privet User1");
+		}
+	}
+}
+		
+	
